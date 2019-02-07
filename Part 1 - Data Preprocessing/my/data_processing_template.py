c6aaf9a225def8ac82a6efc2a44eaca4c340d3d3
vyshakh.jn <vyshakh.jn@PIA3358>
2019-02-07 06:59:03 +0530

    getting harder

--- Part 1 - Data Preprocessing/my/data_processing_template.py
+++ Part 1 - Data Preprocessing/my/data_processing_template.py
@@ -1,10 +1,31 @@
+# Data Preprocessing
 
+# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
+# Importing the dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
-from sklearn.preprocessing
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3]) 
+X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_ = LabelEncoder()
+X[:, 0] = labelEncoder_.fit_transform(X[:, 0])
+onehotencoder = OneHotEncoder(categorical_features = [0])   
+X = onehotencoder.fit_transform(X).toarray()
+
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
+
+# Splitting the dataset into training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.5)
